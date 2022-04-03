5d704e11b598b720b889d17da10e3a9b4a48f0ac
Flávio Farias <fsfariaspe@gmail.com>
2022-04-03 20:53:51 -0300

    Exercício 10

---
+++ src/AulaParticular/Exe10.java
@@ -0,0 +1,18 @@
+package AulaParticular;
+
+/*10.	Faça um algoritmo em Java que informa todos os números de 1000 a 1999
+        que quando divididos por 11 obtemos resto = 5.*/
+public class Exe10 {
+    public static void main(String[] args) {
+
+        int num = 1000;
+        while (num < 2000){
+            if (num % 11 == 5){
+                System.out.println(num);
+            }
+            num++;
+        }
+        System.out.println("FIM!");
+
+    }
+}
