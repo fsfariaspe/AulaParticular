4546ca57e0f996c46847e4c5b8d60d20ed539e4b
Flávio Farias <fsfariaspe@gmail.com>
2022-04-03 22:47:11 -0300

    Exercício 12

---
+++ src/AulaParticular/Exe12.java
@@ -0,0 +1,15 @@
+package AulaParticular;
+
+/*12.	Faça um algoritmo em Java que imprima todos os números de 150 a 300.*/
+public class Exe12 {
+    public static void main(String[] args) {
+
+        int num = 150;
+
+        while(num <= 300){
+            System.out.println(num);
+            num++;
+        }
+        System.out.println("Fim!");
+    }
+}
