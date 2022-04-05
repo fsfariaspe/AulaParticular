0ec4c8afc5b565f00874ba1dfbe5ef20c95e2dc8
Flávio Farias <fsfariaspe@gmail.com>
2022-04-05 20:28:25 -0300

    Exercício 17

---
+++ src/AulaParticular/Exe17.java
@@ -0,0 +1,30 @@
+package AulaParticular;
+
+import java.util.Scanner;
+
+/*17.	Faça um algoritmo em Java que ler um número inteiro (lembrar de colocar as informações
+        do que é para ser feito para o usuário), ao final, imprime um triângulo de asteriscos
+        com o tamanho definido pelo número que foi lido. Exemplo:
+        Digite o tamanho para seu triângulo: 3
+        *
+        **
+        ***
+*/
+public class Exe17 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite o tamanho do seu triângulo");
+        int num = ler.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+
+        ler.close();
+        System.out.println("Fim!");
+    }
+}
