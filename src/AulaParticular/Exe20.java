3aead9030c2e15d199c7bb48bc52356582409583
Flávio Farias <fsfariaspe@gmail.com>
2022-04-05 22:22:58 -0300

    Exercício 20

---
+++ src/AulaParticular/Exe20.java
@@ -0,0 +1,15 @@
+package AulaParticular;
+
+/*20.	Faça um algoritmo em Java que escreva um algoritmo para imprimir
+        os 10 primeiros números inteiros maiores que 100. */
+public class Exe20 {
+    public static void main(String[] args) {
+        int num = 100;
+        int cont = 101;
+        while (cont <= (num + 10)){
+            System.out.println(cont);
+            cont++;
+        }
+        System.out.println("Fim!");
+    }
+}
