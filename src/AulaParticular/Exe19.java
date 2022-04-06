5e1b8f154546646b5e1131efa06d176a7f5ec4cf
Flávio Farias <fsfariaspe@gmail.com>
2022-04-05 22:13:52 -0300

    Exercício 19

---
+++ src/AulaParticular/Exe19.java
@@ -0,0 +1,14 @@
+package AulaParticular;
+
+/*19.	Faça um algoritmo em Java que escreva um algoritmo para imprimir os
+        números de 1 (inclusive) a 10 (inclusive) em ordem decrescente. */
+public class Exe19 {
+    public static void main(String[] args) {
+        int num = 10;
+        while (num >= 1){
+            System.out.println(num);
+            num--;
+        }
+        System.out.println("Fim!");
+    }
+}
