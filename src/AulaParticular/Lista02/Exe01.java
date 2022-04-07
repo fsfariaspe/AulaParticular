25e2d26f584bf90ca02fa1b07cfaa6a363d05593
Flávio Farias <fsfariaspe@gmail.com>
2022-04-06 21:01:39 -0300

    Exercício 1 - Lista 02

---
+++ src/AulaParticular/Lista02/Exe01.java
@@ -0,0 +1,24 @@
+package AulaParticular.Lista02;
+
+import java.util.Scanner;
+
+/*1.	Faça um algoritmo em Java que ler um valor (do teclado) e
+        escrever (na tela) o seu antecessor, usando função.*/
+public class Exe01 {
+    public static void main(String[] args) {
+
+        int num = 0;
+        System.out.println("Digite um número: ");
+        num = ler(num);
+        int ant = num - 1;
+
+        System.out.println("O antecessor de " + num + " é " + ant + "\n" + "Fim!");
+
+    }
+    public static int ler (int i) {
+        Scanner sc = new Scanner(System.in);
+        i = sc.nextInt();
+        return i;
+    }
+
+}
