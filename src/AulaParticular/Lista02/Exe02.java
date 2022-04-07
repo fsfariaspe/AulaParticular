e7f0fb3f7b9cbacc8c040c0dfdad3d0e77b9c5d2
Flávio Farias <fsfariaspe@gmail.com>
2022-04-06 21:41:43 -0300

    Exercício 2 - Lista 02

---
+++ src/AulaParticular/Lista02/Exe02.java
@@ -0,0 +1,24 @@
+package AulaParticular.Lista02;
+
+import java.util.Scanner;
+
+/*2.	Faça um algoritmo em Java que ler um valor e em uma função verifica se o número é par ou ímpar.*/
+public class Exe02 {
+    public static void main(String[] args) {
+        System.out.println("Digite um número");
+        int num = 0;
+        num = ler(num);
+
+        if (num % 2 == 0){
+            System.out.println("PAR");
+        } else {
+            System.out.println("ÍMPAR");
+        }
+        System.out.println("Fim!");
+    }
+    public static int ler (int i) {
+        Scanner sc = new Scanner(System.in);
+        i = sc.nextInt();
+        return i;
+    }
+}
